980d6b49a3f9aa0103df12bc779019fb0c9aee79
JesperNordlund <nordlund.jesper@gmail.com>
2017-11-28 11:48:43 +0100

    Add files via upload

--- MySQL/src/LoadDriver.java
+++ MySQL/src/LoadDriver.java
@@ -73,8 +73,8 @@ public class LoadDriver {
         			productSearchSet.getString("ProductID") + " " +
         			productSearchSet.getString("ProductName") + " " +
         			productSearchSet.getString("CategoryName") + " " +
-        			productSearchSet.getInt("UnitPrice") + " " +
-        			productSearchSet.getInt("UnitsInStock"));
+        			productSearchSet.getInt("UnitPrice") + "kr " +
+        			productSearchSet.getInt("UnitsInStock") + "st");
         	
         }
         
